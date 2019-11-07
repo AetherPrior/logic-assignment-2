7073681573af2fbe7fb1825dc893e79cbbf44857
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-11-07 19:54:38 +0530

    day2 : complete task

--- day2/day2.c
+++ day2/day2.c
@@ -1,6 +1,54 @@
-#include "parse_tree.h"
 #include "stack.h"
+#include "parse_tree.h"
+#include <stdlib.h> // for NULL definition
 
-node *nnf(node *root){
-
+/**
+ * Converts a given parse tree to a NNF form ( without any negations except on atoms)
+ * @param node : pointer to a node in the parse tree
+ * @return pointer to the given node, sub parse tree of which is in NNF
+ */
+node *nnf(node *root)
+{
+    if (root->left == NULL && root->right == NULL)
+    {
+        return root;
+    }
+    if (root->ch == '~' && root->left == NULL && root->right->ch == '~' && root->right->left == NULL && root->right->right != NULL)
+    {
+        root=nnf(root->right->right);
+    }
+    else if (root->ch == '^' || root->ch == 'V')
+    {
+        root->left = nnf(root->left);
+        root->right = nnf(root->right);
+    }
+    else if (root->ch == '~' && root->right->ch == '^')
+    {
+        root->ch = 'V';
+        node *temp1 = malloc(sizeof(node));
+        node *temp2 = malloc(sizeof(node));
+        temp1->ch = '~';
+        temp2->ch = '~';
+        temp1->left = NULL;
+        temp2->left = NULL;
+        temp1->right = root->right->left;
+        temp2->right = root->right->right;
+        root->left = nnf(temp1);
+        root->right = nnf(temp2);
+    }
+    else if (root->ch == '~' && root->right->ch == 'V')
+    {
+        root->ch = '^';
+        node *temp1 = malloc(sizeof(node));
+        node *temp2 = malloc(sizeof(node));
+        temp1->ch = '~';
+        temp2->ch = '~';
+        temp1->left = NULL;
+        temp2->left = NULL;
+        temp1->right = root->right->left;
+        temp2->right = root->right->right;
+        root->left = nnf(temp1);
+        root->right = nnf(temp2);
+    }
+    return root;
 }
